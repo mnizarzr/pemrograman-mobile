d5a9bb91c1334c71f0b4fdc3ab7d57de8a21bffa
Muhammad Nizar <mnizarzr@gmail.com>
2023-10-02 00:49:30 +0700

    kegiatan modul 1

---
+++ modul1/kegiatan/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:modul1/components/action_button.dart';
+import 'package:modul1/components/expendable_fab.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Nizar'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _number = 0;
+
+  void _incrementNumber() {
+    setState(() {
+      _number++;
+    });
+  }
+
+  void _decrementNumber() {
+    setState(() {
+      _number--;
+    });
+  }
+
+  void _resetNumber() {
+    setState(() {
+      _number = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_number',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: ExpandableFab(distance: 100, children: [
+        ActionButton(
+          icon: const Icon(Icons.add),
+          onPressed: () => _incrementNumber(),
+        ),
+        ActionButton(
+          icon: const Icon(Icons.remove),
+          onPressed: () => _decrementNumber(),
+        ),
+        ActionButton(
+          icon: const Icon(Icons.restart_alt),
+          onPressed: () => _resetNumber(),
+        ),
+      ]), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
